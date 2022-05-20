ba8917ea46770f79536b66e9f16d5707a64c0d17
barno003Local <beate.arnold@student.hs-rm.de>
2022-05-20 11:17:13 +0200

    Projekt angelegt

---
+++ src/test/java/de/hsrm/mi/swt/ampel/AmpelTest.java
@@ -0,0 +1,14 @@
+package de.hsrm.mi.swt.ampel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AmpelTest {
+	
+	@Test
+	void test_platzhalter() {
+		Ampel ampel = new Ampel();
+	}
+
+}
