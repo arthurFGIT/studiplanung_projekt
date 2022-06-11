297df93678f34531a9546f48e26842a40e0b00ac
barno003 <beate.arnold@student.hs-rm.de>
2022-06-11 10:03:23 +0200

    HelloWorld

---
+++ src/main/java/de/hsrm/mi/swt/HelloWorld.java
@@ -0,0 +1,14 @@
+package de.hsrm.mi.swt;
+
+
+public class HelloWorld {
+    
+    
+    public static void main (String [] args) {
+
+        
+        System.out.println("Hello World");
+
+    }
+
+}
--- src/main/java/de/hsrm/mi/swt/test.java
+++
@@ -1,5 +0,0 @@
-package de.hsrm.mi.swt;
-
-public class test {
-    
-}
