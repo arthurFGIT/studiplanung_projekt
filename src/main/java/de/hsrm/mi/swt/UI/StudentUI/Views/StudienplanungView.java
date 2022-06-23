c3a4dcfc23e8cd5ea8b2f3db1f4d6a893cbfc59c
barno003 <beate.arnold@student.hs-rm.de>
2022-06-23 20:31:36 +0200

    nochmal an Drag&Drop versucht

--- src/main/java/de/hsrm/mi/swt/UI/StudentUI/Views/StudienplanungView.java
+++ src/main/java/de/hsrm/mi/swt/UI/StudentUI/Views/StudienplanungView.java
@@ -15,137 +15,191 @@ import de.hsrm.mi.swt.Anwendungslogik.Studiplanverwaltung.StudienPlan;
 import de.hsrm.mi.swt.Anwendungslogik.Studiplanverwaltung.StudienplanService;
 import de.hsrm.mi.swt.main.App;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.shape.Rectangle;
 
-public class StudienplanungView extends ScrollPane implements PropertyChangeListener{
+public class StudienplanungView extends ScrollPane implements PropertyChangeListener {
 
-    public final String MODUL = "modul";
+	public final String MODUL = "modul";
 
-    private Modul modul; //TODO Test löschen
-    private StudienPlan studienPlan;
-    private Map<Integer, Modul> modulMap;
-    private ModulView modulView;
-    
-    //private Rectangle modul;
-    
+	private Modul modul; // TODO Test löschen
+	private StudienPlan studienPlan;
+	private Map<Integer, Modul> modulMap;
+	private ModulView modulView;
 
-    private ModulService modulService; //später durch Studienplanservice -> lade Plan ersetzen
+	// private Rectangle modul;
 
+	private ModulService modulService; // später durch Studienplanservice -> lade Plan ersetzen
 
-    public StudienplanungView(App app){
+	public StudienplanungView(App app) {
 
-        // Kompetenz kompetenz = new Kompetenz("Test");
+		// Kompetenz kompetenz = new Kompetenz("Test");
 		// ArrayList<Kompetenz> kompetenzListe = new ArrayList<>();
 		// kompetenzListe.add(kompetenz);
 		// Fachsemester fachsemester = new Fachsemester(5, AngebotsIntervall.WISO);
-        // modul = new Modul(1, "Prog", "tolles Modul", 5, kompetenzListe, fachsemester, fachsemester, true, 200, 200); //TODO: rausmachen
-        
-        // modulMap = new HashMap<>();
-        // modulMap.put(2, modul);
-        modulService = app.getModulService();
-        modulMap = modulService.getModulMap();
-        studienPlan = new StudienPlan(modulMap);
-        studienPlan.addPropertyChangeListener(this);
-        System.out.println("Studienplan added observer");
-        System.out.println(modulMap.toString());
-        modul = modulMap.get(1);
-        System.out.println(modul.toString());
-        modulView = new ModulView(modul);
+		// modul = new Modul(1, "Prog", "tolles Modul", 5, kompetenzListe, fachsemester,
+		// fachsemester, true, 200, 200); //TODO: rausmachen
 
-        this.setContent(modulView);
-		
+		// modulMap = new HashMap<>();
+		// modulMap.put(2, modul);
+		modulService = app.getModulService();
+		modulMap = modulService.getModulMap();
+		studienPlan = new StudienPlan(modulMap);
+		studienPlan.addPropertyChangeListener(this);
+		System.out.println("Studienplan added observer");
+		System.out.println(modulMap.toString());
+		modul = modulMap.get(1);
+		System.out.println(modul.toString());
+		modulView = new ModulView(modul);
+
+		this.setContent(modulView);
+
+		initialize();
+
 		// modulV.getOnDragOver();
-    }
-
-    
-
-    protected void modulDragDetected(Event event) {
-        System.out.println("Drag detected");
-		Dragboard dragboard = modulView.startDragAndDrop(TransferMode.COPY);
-		ClipboardContent content = new ClipboardContent();
-		content.putString(MODUL);
-		dragboard.setContent(content);
-		event.consume();
 	}
 
-
-    // @FXML
+	// @FXML
 	// protected void handleKnopfRechts(ActionEvent event) {
-	// 	// alle Kringel nach rechts schieben - UI folgt automatisch
-	// 	for (Modul k : studienPlan) {
-	// 		k.setX(k.getX() + 5);
-	// 	}
+	// // alle Kringel nach rechts schieben - UI folgt automatisch
+	// for (Modul k : studienPlan) {
+	// k.setX(k.getX() + 5);
+	// }
 	// }
 
 	// @FXML
 	// protected void handleKnopfLinks(ActionEvent event) {
-	// 	// alle Kringel nach links schieben - UI folgt automatisch
-	// 	for (Kringel k : spielfeld) {
-	// 		k.setX(k.getX() - 5);
-	// 	}
+	// // alle Kringel nach links schieben - UI folgt automatisch
+	// for (Kringel k : spielfeld) {
+	// k.setX(k.getX() - 5);
+	// }
 	// }
 
-	// @FXML
-	
+	public void initialize() {
+		modulView.setOnDragDetected(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				System.out.println("Drag detected");
+				Dragboard dragboard = modulView.startDragAndDrop(TransferMode.COPY);
+				ClipboardContent content = new ClipboardContent();
+				content.putString(MODUL);
+				dragboard.setContent(content);
+				event.consume();
+			}
+		});
 
-	// @FXML
-	protected void studentUIOnDragOver(DragEvent event) {
-		if (event.getGestureSource() != studienPlan
-				&& (event.getDragboard().hasContent(ModulView.MODULFORMAT) || event.getDragboard().hasString())) {
-			event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-		}
-		event.consume();
+		// modulView.setOnDragOver(new EventHandler<DragEvent>() {
+		// 	@Override
+		// 	public void handle(DragEvent event) {
+		// 		System.out.println("Search Drop Place");
+		// 		if (event.getGestureSource() != studienPlan
+		// 				&& (event.getDragboard().hasContent(ModulView.MODULFORMAT)
+		// 						|| event.getDragboard().hasString())) {
+		// 			event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+		// 		}
+		// 		event.consume();
+		// 	}
+		// });
+
+		modulView.setOnDragOver(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(DragEvent event) {
+				// System.out.println("Search Drop Place");
+				if (event.getDragboard().hasString()) {
+					event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+				}
+				event.consume();
+			}
+		});
+
+		modulView.setOnDragDropped(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(DragEvent event) {
+				Dragboard dragboard = event.getDragboard();
+				boolean success = false;
+				if(dragboard.hasString()){
+					modul = (Modul) dragboard.getContent(ModulView.MODULFORMAT);
+					int x = (int) event.getX();
+					int y = (int) event.getY();
+					modul.setxKoordinate(x);
+					modul.setyKoordinate(y);
+					System.out.println("SpielfeldUI.spielfeldOnDragDropped -> moved kringel " + modul);
+					success = true;
+				}
+				
+				event.setDropCompleted(success);
+				event.consume();
+			}
+		});
+
+		// modulView.setOnDragDropped(new EventHandler<DragEvent>() {
+		// 	@Override
+		// 	public void handle(DragEvent event) {
+		// 		Dragboard dragboard = event.getDragboard();
+		// 		boolean success = false;
+		// 		modul = (Modul) dragboard.getContent(ModulView.MODULFORMAT);
+		// 		int x = (int) event.getX();
+		// 		int y = (int) event.getY();
+		// 		modul.setxKoordinate(x);
+		// 		modul.setyKoordinate(y);
+		// 		System.out.println("SpielfeldUI.spielfeldOnDragDropped -> moved kringel " + modul);
+		// 		success = true;
+		// 		event.setDropCompleted(success);
+		// 		event.consume();
+		// 	}
+		// });
 
 	}
+	
+
 
 	// public int rasterRunden(double x) {
 
-	// 	int rasterbreite = (int) modulV.getWidth() * 2;
-	// 	return (int) x / rasterbreite * rasterbreite;
+	// int rasterbreite = (int) modulV.getWidth() * 2;
+	// return (int) x / rasterbreite * rasterbreite;
 	// }
 
 	// @FXML
 	// protected void spielfeldOnDragDropped(DragEvent event) {
-	// 	System.out.println("Test gfa fds ");
-	// 	Dragboard dragboard = event.getDragboard();
-	// 	boolean success = false;
-		
-	// 	// TODO: Abfrage validierung
-	// 	modul = (Modul) dragboard.getContent(ModulView.MODULFORMAT);
-	// 	System.out.println("SpielfeldUI.spielfeldOnDragDropped - kringel " + modul);
-	// 	int x = (int) event.getX();
-	// 	int y = (int) event.getY();
-	// 	System.out.println(x + " " + y);
-	// 	// Domänenobjekt ändern - UI folgt automatisch
-	// 	modul.setxKoordinate(x);
-	// 	modul.setyKoordinate(y);
-	// 	System.out.println("SpielfeldUI.spielfeldOnDragDropped -> moved Modul " + modul);
-	// 	success = true;
+	// System.out.println("Test gfa fds ");
+	// Dragboard dragboard = event.getDragboard();
+	// boolean success = false;
 
+	// // TODO: Abfrage validierung
+	// modul = (Modul) dragboard.getContent(ModulView.MODULFORMAT);
+	// System.out.println("SpielfeldUI.spielfeldOnDragDropped - kringel " + modul);
+	// int x = (int) event.getX();
+	// int y = (int) event.getY();
+	// System.out.println(x + " " + y);
+	// // Domänenobjekt ändern - UI folgt automatisch
+	// modul.setxKoordinate(x);
+	// modul.setyKoordinate(y);
+	// System.out.println("SpielfeldUI.spielfeldOnDragDropped -> moved Modul " +
+	// modul);
+	// success = true;
 
-	// 	event.setDropCompleted(success);
-	// 	event.consume();
+	// event.setDropCompleted(success);
+	// event.consume();
 	// }
-
 
 	@Override
 	public void propertyChange(PropertyChangeEvent event) {
 		// UI bei Änderung des zugehörigen Domänenobjekts (Spielfeld) aktualisieren
 		// System.out.println("SpielfeldUI.update " + event);
 		// if (event.getPropertyName().equals(StudienPlan.ADD_EVENT)) {
-		// 	Modul b = (Modul) event.getNewValue();
-		// 	ModulView neuesModul = new ModulView(b);
-		// 	AnchorPane ap = (AnchorPane) spielfeldui.getContent();
-		// 	ap.getChildren().add(neuesModul);
+		// Modul b = (Modul) event.getNewValue();
+		// ModulView neuesModul = new ModulView(b);
+		// AnchorPane ap = (AnchorPane) spielfeldui.getContent();
+		// ap.getChildren().add(neuesModul);
 		// } else {
-		// 	throw new IllegalArgumentException("UnbehandeltesEvent " + event);
+		// throw new IllegalArgumentException("UnbehandeltesEvent " + event);
 		// }
 	}
 
-    
 }
